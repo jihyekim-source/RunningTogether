ce2540e3f354137f3336f1d85d55e23cff5f9002
jihyekim-source <71822165+jihyekim-source@users.noreply.github.com>
2020-10-15 20:19:33 +0900

    Add files via upload

---
+++ 3333.py
@@ -0,0 +1,27 @@
+score=66
+
+if score>=90:
+    print("A")
+
+elif score>=80:
+    print("B")
+
+elif score>=70:
+    print("C")
+
+elif score>=60:
+    print("D")
+else:
+    print("F")
+
+A=10
+B=10
+
+if A>B:
+    print(">")
+
+elif A<B:
+    print("<")
+
+elif A==B:
+    print("==")
